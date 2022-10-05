b1faa49feddc6a19e96805f0125d669a60c3c768
lauramagallanes <pasocenturiontours@gmail.com>
2022-10-05 21:06:52 +0000

    Primer commit de ejercicio traffic ligth

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const TrafficLight = () => {
+
+	//declaro los states y estados de inicio
+	const [encendidoRed,setEncendidoRed] = useState ("none");
+  const [encendidoYellow,setEncendidoYellow] = useState ("none");
+  const [encendidoGreen,setEncendidoGreen] = useState ("none");
+	console.log(encendidoRed);
+
+	//Aca declaro la funcion para "prender" las luces con el style de boxshadow para el rojo
+  let prenderRed = () => {
+				
+    if(encendidoRed == "none"){
+      setEncendidoRed("5px 10px 18px 18px white");
+      setEncendidoGreen("none");
+      setEncendidoYellow("none");
+    }else{
+      setEncendidoRed("none");
+    }
+  }
+	
+  //Aca declaro la funcion para "prender" las luces con el style de boxshadow para el amarillo
+  let prenderYellow = () => {
+		
+    if(encendidoYellow == "none"){
+      setEncendidoYellow("5px 10px 18px 18px white");
+      setEncendidoRed("none");
+      setEncendidoGreen("none");
+    }else{
+      setEncendidoYellow("none");
+    }
+  }
+	
+  //Aca declaro la funcion para "prender" las luces con el style de boxshadow para el verde
+  let prenderGreen = () => {
+	
+    if(encendidoGreen == "none"){
+      setEncendidoGreen("5px 10px 18px 18px white");
+      setEncendidoRed("none");
+      setEncendidoYellow("none");
+    }else{
+      setEncendidoGreen("none");
+    }
+	}
+
+	// aca retorno lo que quiero mostrar con sus cambios onClick
+  return (
+    
+    <div style={{width: "3em"}}>
+             
+          <div className="container bg-danger" onClick= {()=> prenderRed()} style={{boxShadow: encendidoRed}}>Rojo</div>
+          <div className="container bg-warning" onClick= {()=> prenderYellow()} style={{boxShadow: encendidoYellow}}>Naranja</div>
+          <div className="container bg-success" onClick= {()=> prenderGreen()} style={{boxShadow: encendidoGreen}}>Verde</div>
+               
+    </div>
+  );
 };
 
-export default Home;
+export default TrafficLight;
